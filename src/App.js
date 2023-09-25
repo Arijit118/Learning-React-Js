fbb3d2ec977fc640e183c116e7d83ea8330bc8cf
Arijit Roy <arijitroy042017@gmail.com>
2023-09-25 21:19:18 +0530

    Understand Basic workflow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-// import logo from './logo.svg';
 import './App.css';
-
+let name="Arijit";
 function App() {
   return (
-    <h1>This is my first React app</h1>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <h1>Hello, {name}</h1>
+    <div className="container">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam magni laudantium quisquam dolor, quia est sit maxime voluptatem exercitationem vel sint necessitatibus voluptatibus a delectus eum optio, architecto assumenda fugit?
+    </div>
+    </>
+    
   );
 }
 
