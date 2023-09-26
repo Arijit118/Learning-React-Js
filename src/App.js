7f8176b2443ee83fd1791dd8cb8a484866840d20
Arijit Roy <arijitroy042017@gmail.com>
2023-09-26 22:12:25 +0530

    add bootstrap & make custom components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-let name="Arijit";
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <h1>Hello, {name}</h1>
-    <div className="container">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam magni laudantium quisquam dolor, quia est sit maxime voluptatem exercitationem vel sint necessitatibus voluptatibus a delectus eum optio, architecto assumenda fugit?
-    </div>
+    <Navbar title="TextUtils" about="About Us"/>
     </>
     
   );
