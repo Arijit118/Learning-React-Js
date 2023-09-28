9847b20428e43c1c4321ae17977ab2daa39f9a94
Arijit Roy <arijitroy042017@gmail.com>
2023-09-28 22:48:45 +0530

    add more functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,36 @@ const handleLowClick = () =>{
     let newText = text.toLowerCase();
     setText(newText);
 }
+const handleSenClick = () =>{
+  // Split the paragraph into individual sentences using periods as the delimiter.
+  let sentences = text.split('. ');
+
+  // Iterate through each sentence and convert it to sentence case.
+  for (let i = 0; i < sentences.length; i++) {
+    let sentence = sentences[i];
+    sentences[i] = sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+  }
+
+  // Rejoin the sentences into a paragraph.
+  var sentenceCaseParagraph = sentences.join('. ');
+
+  // Set text
+  setText(sentenceCaseParagraph);
+}
+const handleClrClick = () =>{
+    //console.log("Upper case clicked");
+    let newText = '';
+    setText(newText);
+}
+const handleCapClick = ()=>{
+    let Words = text.split(' ');
+    for(let i=0; i<Words.length; i++){
+        let word = Words[i];
+        Words[i] = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+    let wordInParagraph = Words.join(' ');
+    setText(wordInParagraph);
+}
 const handleOnChange = (event) =>{
     //console.log("onchange");
     setText(event.target.value);
@@ -23,8 +53,12 @@ const [text, setText] = useState('Enter your text');
         <div className="mb-3">
             <textarea className="form-control" id="myBox" onChange={handleOnChange} value={text} rows="8"></textarea>
         </div>
-        <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-success mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+        <button className="btn btn-success mx-2" onClick={handleUpClick}>Upper case</button>
+        <button className="btn btn-success mx-2" onClick={handleLowClick}>Lower case</button>
+        <button className="btn btn-success mx-2" onClick={handleSenClick}>Sentence case</button>
+        <button className="btn btn-success mx-2" onClick={handleCapClick}>Capitalized case</button>
+        <button className="btn btn-success mx-2" onClick={handleClrClick}>Clear</button>
+
     </div>
     <div className='container mt-5'>
         <h1>Your text summary</h1>
